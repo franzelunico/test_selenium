c6e47bd5e6ad6c5edeb51f631c3d6b255c18e562
Franz Lopez <franzelunico@gmail.com>
2016-10-03 20:06:04 -0400

    display react emotions

--- prueba.py
+++ prueba.py
@@ -1,35 +1,63 @@
 import time
 from selenium import webdriver
 
-# Login
-driver = webdriver.Chrome('/Users/codebo04/Downloads/chromedriver')
-driver.get('https://www.facebook.com/login.php')
-search_box = driver.find_element_by_name('email')
-search_box.send_keys('email')
-search_box1 = driver.find_element_by_name('pass')
-search_box1.send_keys('password')
-search_box1.submit()
-# Page scrapin
-driver.get('https://www.facebook.com/thehackernews/')
-thelist = []
-# get Post
-for item in driver.find_elements_by_tag_name('a'):
-    ahref = item.get_attribute("href")
-    if ahref is not None and ahref.find("https://www.facebook.com/ufi/") > -1:
-        thelist.append(item.get_attribute("href"))
-driver.get(thelist[0])
-time.sleep(30)
-driver.quit()
 
-# driver.get('https://www.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier=1538171092863425&av=100012901368010')
-# # login_form = driver.find_element_by_xpath("//div[@class='_5j0e fsl fwb fcb']/a")
-# element2 = driver.find_element_by_xpath("//ul[@class='uiList _5i_n _4kg _6-h _6-j _6-i']")
-# print type(element2)
-# print len(element2)
-# print element2.text
-# print help(element2.text)
-# # element2.find_element_by_xpath(".//p[@class='test']").text 
-# # for item in driver.find_element_by_xpath('//a[contains(@href,"profile.php"]'):
-# #     print item
-# time.sleep(30)
-# driver.quit()
+class ScrapingPage(object):
+    def __init__(self):
+        print "Hello"
+
+    def user(self):
+        file = open('data.txt')
+        lines = file.read().splitlines()
+        self.name = lines[0]
+        self.password = lines[1]
+        file.close()
+
+    def loadDriver(self):
+        path = './chromeDriver/chromedriver'
+        # self.driver = webdriver.Chrome(path)
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(executable_path=path,
+                                       chrome_options=chrome_options)
+
+    def login(self):
+        self.driver.get('https://www.facebook.com/login.php/')
+        username = self.driver.find_element_by_name('email')
+        username.send_keys(self.name)
+
+        password = self.driver.find_element_by_name('pass')
+        password.send_keys(self.password)
+
+        login_button = self.driver.find_element_by_name('login')
+        login_button.click()
+
+    def scraping(self):
+        self.driver.get('https://www.facebook.com/thehackernews/')
+        thelist = []
+        # get Post
+        for item in self.driver.find_elements_by_tag_name('a'):
+            ahref = item.get_attribute("href")
+            links = "https://www.facebook.com/ufi/"
+            if ahref is not None and ahref.find(links) > -1:
+                thelist.append(item.get_attribute("href"))
+        self.driver.get(thelist[0])
+
+    def despliegue(self):
+        css = 'uiMorePagerPrimary'
+        for item in self.driver.find_elements_by_class_name(css):
+            item.click()
+            time.sleep(5)
+
+    def run(self):
+        self.user()
+        self.loadDriver()
+        self.login()
+        self.scraping()
+        time.sleep(5)
+        self.despliegue()
+
+if __name__ == "__main__":
+    app = ScrapingPage()
+    app.run()
